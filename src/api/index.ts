88e1a990d8d7cf437cd660c25fb76b667f56d0d4
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2019-09-07 00:47:43 +0200

    :hammer: Handle not finding XMPP services

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import discoverXMPPServices from 'xmpp-connection-discovery';
+import isEmpty from 'lodash.isempty';
 
 export default () => {
   const api = Router();
@@ -8,17 +9,14 @@ export default () => {
     const { domain } = req.params;
 
     try {
-      /**
-       *  FIXME: Update xmpp-connection-discovery package version
-       *  when the publisher updates npm with the most recent code
-       *  available on GitHub.
-       *
-       *  When an invalid domain is supplied,
-       *  xmpp-connection-discovery does not behave nicely.
-       *  This behavior is fixed on GitHub but not published
-       *  on npm.
-       */
       const services = await discoverXMPPServices(domain);
+
+      if (isEmpty(services)) {
+        return res.status(404).json({
+          error: { message: 'XMPP Services Not Found' },
+          data: null,
+        });
+      }
 
       return res.json({
         error: null,
